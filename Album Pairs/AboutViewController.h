381cee12a8fbee6f9af864151bf14985e49def94
Iain Mullan <iain.mullan@gmail.com>
2013-12-17 14:28:29 +0000

    add about view

---
+++ Album Pairs/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Album Pairs
+//
+//  Created by Iain Mullan on 17/12/2013.
+//  Copyright (c) 2013 Iain Mullan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ Album Pairs/AboutViewController.m
@@ -0,0 +1,45 @@
+//
+//  AboutViewController.m
+//  Album Pairs
+//
+//  Created by Iain Mullan on 17/12/2013.
+//  Copyright (c) 2013 Iain Mullan. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *contentView;
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"about" ofType:@"html"];
+    NSString *htmlText = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+
+    [self.contentView loadHTMLString:htmlText baseURL:nil];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
